2b618658c9f5063e296e435b27d43ddb766900a6
Rohit Anantha <anantha.rohit.11@gmail.com>
2024-11-13 10:47:18 -0600

    fixed honey char issues

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 from flask import Flask
 import random
-import ollama
 from flask_cors import CORS
 import time
 
@@ -25,7 +24,7 @@ def get_words():
     possible_words = set()
     current_time = time.gmtime()
     ct = current_time.tm_year * 10000 + current_time.tm_mon * 100 + current_time.tm_mday
-    print(ct)
+    honey_c = None
     while len(possible_words) < 20:
         letters = list("qwrtypsdfghjklzxcvbnm")
         aeiou = list("aeiou")
@@ -35,43 +34,27 @@ def get_words():
         random.shuffle(aeiou)
         chosen = letters[:5]
         chosen.extend(aeiou[:2])
+        honey_c = aeiou[0]
         chosen_set = set(chosen)
         empty = set()
         for word in wordset:
             if set(list(word)).difference(chosen_set) == empty:
-                if aeiou[1] in word:
+                if honey_c in word:
                     possible_words.add(word)
         ct *= 2
     
     sorted_chosen = letters[:3] + aeiou[:2] + letters[3:5]
-    possible_words = list(possible_words)
-    sorted_chosen = [x.upper() for x in sorted_chosen]
     possible_words = [x.upper() for x in possible_words]
-    print(chosen, sorted_chosen)
-    json = {"chosen": sorted_chosen, "possible_words": possible_words}
+    json = {"chosen": sorted_chosen, "possible_words": possible_words, "honey_char": honey_c}
     return json
-
-
-def get_hint(word):
-    response = ollama.chat(
-        model="llama3.1",
-        messages=[
-            {
-                "role": "system",
-                "content": "Reply with the word and a 1 sentence clue about the word, nothing else.",
-            },
-            {"role": "user", "content": word},
-        ],
-    )
-    return response
-
 
 if __name__ == "__main__":
     json = get_words()
     possible_words = json['possible_words']
     chosen = json['chosen']
+    honey_c = json['honey_char']
     possible_words = sorted(list(possible_words), key=lambda x: len(x), reverse=True)
-    print(possible_words)
+    print(possible_words, chosen, honey_c)
 
 @app.route("/api")
 def api():
